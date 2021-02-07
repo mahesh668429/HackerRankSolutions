72ddede1d658a1d2684ed34bebcb410e3d0538cc
Mahesh <maheshpydev@gmail.com>
2021-02-07 11:52:56 -0500

    adding comments

--- magicsquare.py
+++ magicsquare.py
@@ -8,8 +8,11 @@ from itertools import *
 def formingmagicsquare(s):
     ans = 81
     for P in permutations(range(1, 10)):
+        '''permutations fun return all permutation of the elements in a given iterable '''
         if sum(P[0:3]) == sum(P[3:6]) == sum(P[0::3]) == sum(P[1::3]) == (P[0] + P[4] + P[8]) == (
                 P[2] + P[4] + P[6]) == 15:
+            '''[x:y] means get the elements from x to y index excluding y
+               [x::y] means get the the elements from x to end of the list with an interval of y '''
             ans = min(ans, sum(abs(P[i] - s[i]) for i in range(0, 9)))
     return ans
 
